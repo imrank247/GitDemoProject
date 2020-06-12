657f7f22c0f13a1cdebaa80344c13161f7fff9f3
cooli <cooli@ifte>
2020-06-12 12:37:30 -0400

    FIrst commit...

---
+++ src/gitPack/Test1.java
@@ -0,0 +1,15 @@
+package gitPack;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String hurry = "Yes";
+
+		if (hurry == "Yes")
+			System.out.println("eat something");
+		else
+			System.out.println("do work");
+	}
+
+}
